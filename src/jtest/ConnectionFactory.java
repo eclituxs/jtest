15ce167a15fcaeac7eace3978bb55cbd309cf87a
ShaoZePeneg <battle_start@126.com>
2017-02-13 11:41:43 +0800

    modify connectionFactory to project

--- src/jtest/ConnectionFactory.java
+++ src/jtest/ConnectionFactory.java
@@ -1,5 +1,24 @@
 package jtest;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class ConnectionFactory {
-
+	public Connection getConnection(){
+		Connection conn = null;
+		String user ="root";
+		String pwd = "root";
+		String myjdbc = "jdbc:mysql://localhost:3306/studb";
+		try{
+			Class.forName("com.mysql.jdbc.Driver");
+		}catch(ClassNotFoundException ce){
+			System.out.println(ce);
+		}
+		try{
+			conn = DriverManager.getConnection(myjdbc,user,pwd);
+		}catch(SQLException ce){
+			System.out.println(ce);
+		}
+		return conn;
+	}
 }
